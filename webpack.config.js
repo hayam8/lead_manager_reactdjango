133f16480a77e8b2f297b0aed58bfefdda11a4d0
hayam8 <hayam8@uw.edu>
2019-08-03 15:02:09 -0700

    React components

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+// use of babel to transpire code
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
+  }
+};
