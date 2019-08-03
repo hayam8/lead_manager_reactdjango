cbe36ecc40b9fa9185f85660535830d08bcae10a
hayam8 <hayam8@uw.edu>
2019-08-03 16:36:25 -0700

    ADD_LEAD action, form, submit

--- leadmanager/frontend/src/actions/leads.js
+++ leadmanager/frontend/src/actions/leads.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
-import { GET_LEADS, DELETE_LEAD } from "./types";
+import { GET_LEADS, DELETE_LEAD, ADD_LEAD } from "./types";
 
-// action method for GET LEADS called in leads component
+// action method for GET LEADS with get request called in leads component
 export const getLeads = () => dispatch => {
   axios
     .get("/api/leads/")
@@ -15,7 +15,7 @@ export const getLeads = () => dispatch => {
     .catch(err => console.log(err));
 };
 
-// Action method to delete a lead
+// Action method to delete a lead with delete request
 export const deleteLead = id => dispatch => {
   axios
     .delete(`/api/leads/${id}/`)
@@ -23,6 +23,19 @@ export const deleteLead = id => dispatch => {
       dispatch({
         type: DELETE_LEAD,
         payload: id
+      });
+    })
+    .catch(err => console.log(err));
+};
+
+// Action method to ADD a LEAD with post request
+export const addLead = lead => dispatch => {
+  axios
+    .post("/api/leads/", lead)
+    .then(res => {
+      dispatch({
+        type: ADD_LEAD,
+        payload: res.data
       });
     })
     .catch(err => console.log(err));
